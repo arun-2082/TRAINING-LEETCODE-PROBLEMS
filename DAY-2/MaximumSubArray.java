2f5d872cd11f39988777f6375c7fd712684cc10d
arun-2082 <109230756+arun-2082@users.noreply.github.com>
2022-12-17 19:04:33 +0530

    Create MaximumSubArray.java

---
+++ DAY-2/MaximumSubArray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int s = 0;
+        int maximum = nums[0];
+        for(int i=0;i<nums.length;i++){
+            s += nums[i];
+            if(s>maximum){
+                maximum = s;
+            }
+            if(s<0){
+                s=0;
+            }
+        }
+        return maximum;
+        
+    }
+}
